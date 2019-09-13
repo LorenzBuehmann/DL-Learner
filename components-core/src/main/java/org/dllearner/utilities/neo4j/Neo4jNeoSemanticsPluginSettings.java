68550654b04e84ba89679f679fac98e4583822af
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2019-09-13 10:54:00 +0200

    ext. Cypher conn

--- components-core/src/main/java/org/dllearner/utilities/neo4j/Neo4jNeoSemanticsPluginSettings.java
+++ components-core/src/main/java/org/dllearner/utilities/neo4j/Neo4jNeoSemanticsPluginSettings.java
@@ -3,8 +3,9 @@ package org.dllearner.utilities.neo4j;
 /**
  * Contains the settings around the Neosemantic plugin for Neo4J.
  *
+ * @author Lorenz Buehmann
  */
-public class NeoSemanticsPluginSettings {
+public class Neo4jNeoSemanticsPluginSettings {
 
     private static String DEFAULT_RESOURCE_LABEL = "Resource";
     private static String DEFAULT_CLASS_LABEL = "Class";
@@ -15,7 +16,7 @@ public class NeoSemanticsPluginSettings {
     private static String DEFAULT_DOMAIN_REL = "DOMAIN";
     private static String DEFAULT_RANGE_REL = "RANGE";
 
-    public static final NeoSemanticsPluginSettings STANDARD = new NeoSemanticsPluginSettings.Builder().build();
+    public static final Neo4jNeoSemanticsPluginSettings STANDARD = new Neo4jNeoSemanticsPluginSettings.Builder().build();
 
     private String resourceLabel;
     private String classLabel;
@@ -28,13 +29,13 @@ public class NeoSemanticsPluginSettings {
 
     private boolean keepCustomDatatypes = false;
 
-    private NeoSemanticsPluginSettings(String resourceLabel,
-                                       String classLabel,
-                                       String objectPropertyLabel,
-                                       String dataPropertyLabel,
-                                       String subClassOfRel, String subPropertyOfRel,
-                                       String domainRel, String rangeRel,
-                                       boolean keepCustomDatatypes) {
+    private Neo4jNeoSemanticsPluginSettings(String resourceLabel,
+                                            String classLabel,
+                                            String objectPropertyLabel,
+                                            String dataPropertyLabel,
+                                            String subClassOfRel, String subPropertyOfRel,
+                                            String domainRel, String rangeRel,
+                                            boolean keepCustomDatatypes) {
         this.resourceLabel = resourceLabel;
         this.classLabel = classLabel;
         this.objectPropertyLabel = objectPropertyLabel;
@@ -84,64 +85,64 @@ public class NeoSemanticsPluginSettings {
 
     public static class Builder {
 
-        private String resourceLabel = NeoSemanticsPluginSettings.DEFAULT_RESOURCE_LABEL;
-        private String classLabel = NeoSemanticsPluginSettings.DEFAULT_CLASS_LABEL;
-        private String objectPropertyLabel = NeoSemanticsPluginSettings.DEFAULT_OBJECT_PROPERTY_LABEL;
-        private String dataPropertyLabel = NeoSemanticsPluginSettings.DEFAULT_DATA_PROPERTY_LABEL;
-        private String subClassOfRel = NeoSemanticsPluginSettings.DEFAULT_SUBCLASSOF_REL;
-        private String subPropertyOfRel = NeoSemanticsPluginSettings.DEFAULT_SUBPROPERTYOF_REL;
-        private String domainRel = NeoSemanticsPluginSettings.DEFAULT_DOMAIN_REL;
-        private String rangeRel = NeoSemanticsPluginSettings.DEFAULT_RANGE_REL;
+        private String resourceLabel = Neo4jNeoSemanticsPluginSettings.DEFAULT_RESOURCE_LABEL;
+        private String classLabel = Neo4jNeoSemanticsPluginSettings.DEFAULT_CLASS_LABEL;
+        private String objectPropertyLabel = Neo4jNeoSemanticsPluginSettings.DEFAULT_OBJECT_PROPERTY_LABEL;
+        private String dataPropertyLabel = Neo4jNeoSemanticsPluginSettings.DEFAULT_DATA_PROPERTY_LABEL;
+        private String subClassOfRel = Neo4jNeoSemanticsPluginSettings.DEFAULT_SUBCLASSOF_REL;
+        private String subPropertyOfRel = Neo4jNeoSemanticsPluginSettings.DEFAULT_SUBPROPERTYOF_REL;
+        private String domainRel = Neo4jNeoSemanticsPluginSettings.DEFAULT_DOMAIN_REL;
+        private String rangeRel = Neo4jNeoSemanticsPluginSettings.DEFAULT_RANGE_REL;
 
         private boolean keepCustomDatatypes = false;
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setResourceLabel(String resourceLabel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setResourceLabel(String resourceLabel) {
             this.resourceLabel = resourceLabel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setClassLabel(String classLabel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setClassLabel(String classLabel) {
             this.classLabel = classLabel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setObjectPropertyLabel(String objectPropertyLabel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setObjectPropertyLabel(String objectPropertyLabel) {
             this.objectPropertyLabel = objectPropertyLabel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setDataPropertyLabel(String dataPropertyLabel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setDataPropertyLabel(String dataPropertyLabel) {
             this.dataPropertyLabel = dataPropertyLabel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setSubClassOfRel(String subClassOfRel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setSubClassOfRel(String subClassOfRel) {
             this.subClassOfRel = subClassOfRel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setSubPropertyOfRel(String subPropertyOfRel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setSubPropertyOfRel(String subPropertyOfRel) {
             this.subPropertyOfRel = subPropertyOfRel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setDomainRel(String domainRel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setDomainRel(String domainRel) {
             this.domainRel = domainRel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setRangeRel(String rangeRel) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setRangeRel(String rangeRel) {
             this.rangeRel = rangeRel;
             return this;
         }
 
-        public org.dllearner.utilities.neo4j.NeoSemanticsPluginSettings.Builder setKeepCustomDatatypes(boolean keepCustomDatatypes) {
+        public Neo4jNeoSemanticsPluginSettings.Builder setKeepCustomDatatypes(boolean keepCustomDatatypes) {
             this.keepCustomDatatypes = keepCustomDatatypes;
             return this;
         }
 
-        NeoSemanticsPluginSettings build() {
-            return new NeoSemanticsPluginSettings(resourceLabel, classLabel, objectPropertyLabel, dataPropertyLabel, subClassOfRel, subPropertyOfRel, domainRel, rangeRel, keepCustomDatatypes);
+        Neo4jNeoSemanticsPluginSettings build() {
+            return new Neo4jNeoSemanticsPluginSettings(resourceLabel, classLabel, objectPropertyLabel, dataPropertyLabel, subClassOfRel, subPropertyOfRel, domainRel, rangeRel, keepCustomDatatypes);
         }
     }
 
